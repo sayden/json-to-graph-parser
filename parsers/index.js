d3aff56261363c58541d1c8c205d1df8a67eff85
Mario Castro <mariocaster@gmail.com>
2016-02-14 16:52:04 +0100

    extraction algorithm improved

--- parsers/index.js
+++ parsers/index.js
@@ -1,19 +1,33 @@
 module.exports = {
-  parse: function(o, origin, printer, finishCallback){
+  parse: function(origin, o, printer, finishCallback){
+    var self = this;
     for(var k in o){
       if(o.hasOwnProperty(k)){
-        if( Object.prototype.toString.call( o[k] ) === '[object Object]' ) {
-          printer(origin, null, k);
-          this.parse(o[k], k, printer);
+        if( Object.prototype.toString.call( o[k] ) === '[object Object]') {
+          // console.log("1:", origin, k, o[k])
+          this.parse(origin, k, printer);
+          this.parse(k, o[k], printer);
+
+        } else if(Object.prototype.toString.call( o[k] ) === '[object Array]') {
+          // console.log("2:", origin, k, o[k])
+          this.parse(origin, k, printer);
+          o[k].forEach(function(arObject){
+            var temp = {};
+            temp[k] = arObject;
+            self.parse(k, arObject, printer);
+          });
+
         } else {
-          if(o[k].toString().indexOf(",") != -1){
-            o[k].forEach(function(i){
-              printer(origin, k, i);
-            })
+          if(k == 0){
+            // console.log("3.1:", origin, k, o[k], ", print:", origin, "has", o[k])
+            printer(origin, "has", o[k]);
           } else {
-            printer(origin, k, o[k]);
+            // console.log("3.2:", origin, k, o[k], ", print:", origin, "has", k, ",", k, "has", o[k])
+            printer(origin, "has", k);
+            printer(k, "has", o[k]);
           }
         }
+
       }
     }
     if (finishCallback !== undefined){
