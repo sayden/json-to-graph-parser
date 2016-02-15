5438d32b86349ca6bfccdf49fac122a47d6bf415
Mario Castro <mariocaster@gmail.com>
2016-02-15 22:19:42 +0100

    improved test files

--- test/jsonTestFiles.js
+++ test/jsonTestFiles.js
@@ -1,59 +1,28 @@
 module.exports = {
   simple: {
-    a:"b",
-    c:["d", "e", "f"],
-    g:{
-      h:"i",
-      j:["k"],
-      n:{
-        o:"p",
-        q:"v",
-        w:{x:"y"}
-      }
+    A:"B",
+    C:["D", "E"],
+    F:{
+      G:"H",
+      I:["J","K"],
+      L:[
+        {M:"N",O:"P"}
+      ]
     },
-    z:[
+    Z:[
       {
-        asdf:"qwer"
+        ASDF:"QWER1",
+        Q:["R","S"]
+      },
+      {
+        TYURY:"CBNVBN"
       }
     ]
   },
 
-  complex: {
-    Mario:{
-      loves: "Ula",
-      likes: ["Tyrion", "Tesla", "movies"],
-      born_in: "january",
-      "work_in": "madrid"
-    },
-    Ula:{
-      loves: "Tyrion",
-      likes: ["horses", "movies"],
-      born_in: "february",
-      "work_in": "madrid"
-    },
-    Tyrion:{
-      loves: "everything",
-      likes: ["Ula", "Mario", "running", "food"],
-      born_in: "June",
-      "work_in": "home"
-    },
-    Tesla:{
-      loves:"leave_me_alone",
-      likes:["Tesla", "food"],
-      born_in: "June",
-      "work_in": "home"
-    },
-    Ola:{
-      loves:["Pawel", "Gosia", "Ula", "Mario"],
-      likes:["food", "candies", "to_laugh"],
-      born_in:"october",
-      "work_in": "home"
-    },
-    Rocco:{
-      loves:["sleep"],
-      likes:["Mario", "Ula", "Maria", "cuca", "get_angry"],
-      born_in:"february",
-      "work_in": "home"
-    }
+  complex:{
+    Mario:[
+      {likes:"Ula"}
+    ]
   }
 }
